72cee40f5803ba8f872ba2207c8a31d859bfd14b
Chandan <cmahato2000@gmail.com>
2024-01-17 20:49:57 +0545

    cs ce drive link tree

---
+++ index.js
@@ -0,0 +1,48 @@
+//select acnhor element
+let audio1 = document.querySelector(".audio1");
+let audio2 = document.querySelector(".audio2");
+let audio3 = document.querySelector(".audio3");
+let audio4 = document.querySelector(".audio4");
+let audio5 = document.querySelector(".audio5");
+let audio6 = document.querySelector(".audio6");
+let audio7 = document.querySelector(".audio7");
+let audio8 = document.querySelector(".audio8");
+let audio9 = document.querySelector(".audio9");
+let audio10 = document.querySelector(".audio10");
+let audio11 = document.querySelector(".audio11");
+
+let audioArr1 = document.getElementsByTagName("audio");
+//Add event listeners
+audio1.addEventListener("mouseenter", () => {
+  audioArr1[0].play();
+});
+audio2.addEventListener("mouseenter", () => {
+  audioArr1[1].play();
+});
+audio3.addEventListener("mouseenter", () => {
+  audioArr1[2].play();
+});
+audio4.addEventListener("mouseenter", () => {
+  audioArr1[3].play();
+});
+audio5.addEventListener("mouseenter", () => {
+  audioArr1[4].play();
+});
+audio6.addEventListener("mouseenter", () => {
+  audioArr1[5].play();
+});
+audio7.addEventListener("mouseenter", () => {
+  audioArr1[6].play();
+});
+audio8.addEventListener("mouseenter", () => {
+  audioArr1[7].play();
+});
+audio9.addEventListener("mouseenter", () => {
+  audioArr1[8].play();
+});
+audio10.addEventListener("mouseenter", () => {
+  audioArr1[9].play();
+});
+audio11.addEventListener("mouseenter", () => {
+  audioArr1[10].play();
+});
